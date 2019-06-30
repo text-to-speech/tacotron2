0ac25e5afa063ea5fb9edbb2a8754684c993007c
Leggera <lana.krlv1995@gmail.com>
2019-06-30 21:07:53 +0300

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -25,8 +25,9 @@ def create_hparams(hparams_string=None, verbose=False):
         # Data Parameters             #
         ################################
         load_mel_from_disk=False,
-        training_files='/content/gdrive/My Drive/Research/data/train_split.txt',
-        validation_files='/content/gdrive/My Drive/Research/data/val_split.txt',
+        mel_path = '/content/gdrive/My Drive/Research/data/',
+        training_files='train.txt',
+        validation_files='val.txt',
         text_cleaners=['english_cleaners'],
 
         ################################
